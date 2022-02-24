81c63f22b1310f2ca5d78d2c79fba50cd2b426b1
KyleHu14 <lloydpearce4@gmail.com>
2022-02-24 13:28:09 -0800

    added del btn functionality

--- client/src/components/DisplayVideo.js
+++ client/src/components/DisplayVideo.js
@@ -9,6 +9,16 @@ const DisplayVideo = () => {
         setVideos(videoArr);
     }
 
+    async function delVideo(id) {
+        try {
+            const res = await fetch(`http://localhost:5000/videos/${id}`, {
+                method: "DELETE",
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     useEffect(() => {
         getVideos();
     });
@@ -18,8 +28,19 @@ const DisplayVideo = () => {
             <div className="task-list">
                 {videos.map((video) => (
                     <div key={video.id} className="task">
-                        <div className="title">{video.video_title}</div>
-                        Link : {video.video_link}
+                        <div className="text">
+                            <div className="title">{video.video_title}</div>
+                            Link : {video.video_link}
+                        </div>
+                        <div className="buttons">
+                            <button
+                                className="btn-delete"
+                                onClick={() => delVideo(video.id)}
+                            >
+                                Delete
+                            </button>
+                            <button className="btn-edit">Edit</button>
+                        </div>
                     </div>
                 ))}
             </div>
